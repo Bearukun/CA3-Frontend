8a7f63475dede844c6a879ed2ad9ad1e919b78c6
hannibal <hanni.skriver@gmail.com>
2017-04-05 18:23:18 +0200

    AdminPage updated

--- src/pages/AdminPage.js
+++ src/pages/AdminPage.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import {observer} from "mobx-react";
-import UserStore from "../stores/adminStore";
+import AdminStore from "../stores/adminStore";
+import auth from '../authorization/auth';
 
 const AdminPage = observer(
   class AdminPage extends Component {
@@ -10,18 +11,39 @@ const AdminPage = observer(
       This will fetch data each time you navigate to this route
       Move to constructor, if only required once, or add "logic" to determine when data should be "refetched"
       */
-        UserStore.getData();
+        AdminStore.getData();
     }
 
     render() {
+
+        // var lis = AdminStore.users.map(function(user){
+        //     return(
+        //         <li key={user.userName}>{user.role}</li>
+        //     )
+        // })
+          var lis =  AdminStore.users.map(function(user) {
+              return (
+                  <li key={user.userName}> {user.userName}</li>
+              )
+            })
+
       return (
         <div>
           <h2>User administration</h2>
-          <p>Hej!</p>
+
           <div className="msgFromServer">
-          {UserStore.messageFromServer}
+              {/*{auth.loggedIn && auth.isAdmin?//only show if logged in and a user*/}
+                  {/*<ul>{lis}</ul>*/}
+                  {/*: null}*/}
+              <ul>{lis}</ul>
+          {/*{AdminStore.messageFromServer}*/}
+
+
+
+              {/*{AdminStore.messageFromServer}*/}
+
           </div>
-          <h4 style={{color: "red"}}>{UserStore.errorMessage}</h4>
+          <h4 style={{color: "red"}}>{AdminStore.errorMessage}</h4>
         </div>
       )
     }
--- src/stores/adminStore.js
+++ src/stores/adminStore.js
@@ -1,12 +1,14 @@
 
 import { observable, action } from "mobx";
 import fetchHelper from "./fetchHelpers"
+import {computed} from "../../node_modules/mobx/lib/mobx";
 const URL = require("../../package.json").serverURL;
 
 /* encapsulates Data related to Admins */
 class AdminStore {
   @observable messageFromServer = "";
   @observable errorMessage = "";
+  @observable _users = [];
 
   @action
   setErrorMessage(err) {
@@ -17,6 +19,43 @@ class AdminStore {
     this.messageFromServer = msg;
   }
 
+
+    // @action
+    // getUsers = () => {
+    //     this.errorMessage = "";
+    //     this.messageFromServer = "";
+    //     let errorCode = 200;
+    //     const options = fetchHelper.makeOptions("GET", true);
+    //     var conf = {
+    //         method: 'get',
+    //         headers: {
+    //             'Accept': 'application/json',
+    //             'Content-Type': 'application/json',
+    //             'Access-Control-Allow-Origin': '*',
+    //             'Authorization': 'Admin'
+    //         }
+    //     };
+    //     // fetch(URL + "api/demoadmin/all", options)
+    //     fetch(URL + "api/demoadmin", options)
+    //         .then((res) => {
+    //             if (res.status > 210 || !res.ok) {
+    //                 errorCode = res.status;
+    //             }
+    //             return res.json();
+    //         })
+    //         .then(action((res) => {  //Note the action wrapper to allow for useStrict
+    //             if (errorCode !== 200) {
+    //                 throw new Error(`${res.error.message} (${res.error.code})`);
+    //             }
+    //             else {
+    //                 this._users.replace(res);
+    //             }
+    //         })).catch(err => {
+    //         //This is the only way (I have found) to verify server is not running
+    //         this.setErrorMessage(fetchHelper.addJustErrorMessage(err));
+    //     })
+    // }
+    //
   @action
   getData = () => {
     this.errorMessage = "";
@@ -35,13 +74,45 @@ class AdminStore {
           throw new Error(`${res.error.message} (${res.error.code})`);
         }
         else {
-          this.setMessageFromServer(res.message);
+
+
+          this._users.replace(res);
         }
       }).catch(err => {
         //This is the only way (I have found) to veryfy server is not running
         this.setErrorMessage(fetchHelper.addJustErrorMessage(err));
       })
   }
+    @computed
+    get users(){
+        return this._users;
+    }
+  //   @action
+  //   getData = () => {
+  //       this.errorMessage = "";
+  //       this.messageFromServer = "";
+  //       let errorCode = 200;
+  //       const options = fetchHelper.makeOptions("GET", true);
+  //       fetch(URL + "api/demoadmin", options)
+  //           .then((res) => {
+  //               if (res.status > 200 || !res.ok) {
+  //                   errorCode = res.status;
+  //               }
+  //               return res.json();
+  //           })
+  //           .then((res) => {
+  //               if (errorCode !== 200) {
+  //                   throw new Error(`${res.error.message} (${res.error.code})`);
+  //               }
+  //               else {
+  //                   this.setMessageFromServer(res.message);
+  //               }
+  //           }).catch(err => {
+  //           //This is the only way (I have found) to veryfy server is not running
+  //           this.setErrorMessage(fetchHelper.addJustErrorMessage(err));
+  //       })
+  //   }
+
 }
 let adminStore = new AdminStore(URL);
 
